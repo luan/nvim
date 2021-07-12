1f2324417681a13d1d63b1ff015ac2c1317c6122
Luan Santos <git@luan.sh>
2021-07-11 17:01:06 -0700

    user settings hook

---
+++ lua/user.defaults/plugins.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+M.plugins = function(use)
+  -- Add your own plugins
+  -- use 'kyazdani42/nvim-tree.lua'
+  -- use '~/my-prototype-plugin'
+  -- see :help packer for more options
+end
+
+return M
