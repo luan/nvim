089eddcd6bdf47a3592c7c1a2766501444aa046a
Luan Santos <cfcluan@gmail.com>
2021-07-11 19:51:51 -0700

    configure lualine secrions properly

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,28 +3,34 @@ require('lualine').setup {
         theme = 'neon',
         section_separators = {'', ''},
         component_separators = {'', ''},
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch'},
-            lualine_c = {
-                'filetype',
-                {
-                    'filename',
-                    file_status = true,
-                    path = 1,
-                },
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {
+            'filetype',
+            {
+                'filename',
+                file_status = true,
+                path = 1,
             },
-            lualine_x = {'diagnostics', 'encoding', 'fileformat'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
         },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {},
+        lualine_x = {'diagnostics', 'encoding', 'fileformat'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+            {
+                'filename',
+                file_status = true,
+                path = 1,
+            },
         },
-    }
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {},
+    },
 }
