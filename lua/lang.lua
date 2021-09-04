6d5f2ba68b7d8ed34645225ac27303f9abfc84f2
Luan Santos <luan@dropbox.com>
2021-09-03 17:49:32 -0700

    fix solargraph path

--- lua/lang.lua
+++ lua/lang.lua
@@ -1,4 +1,6 @@
 local lsp_installer = require("nvim-lsp-installer")
+local path = require("nvim-lsp-installer.path")
+local lsp_server = require "nvim-lsp-installer.server"
 
 lsp_installer.on_server_ready(function(server)
   local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -36,6 +38,9 @@ lsp_installer.on_server_ready(function(server)
         staticcheck = true,
       },
     }
+  elseif server.name == 'solargraph' then
+    local root_dir = lsp_server.get_server_root_path("ruby")
+    opts.cmd = { path.concat { root_dir, "solargraph"  }, "stdio" }
   end
 
   server:setup(opts)
