5320c9aeddad2bb0318a05e4c12601dd5ced794e
Luan Santos <luan@dropbox.com>
2021-10-06 14:04:44 -0700

    allow restarting gopls

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -9,11 +9,12 @@ function GoImportsSync(timeout_ms)
     params.context = context
 
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-    if not result then return end
-    result = result[1].result
-    if not result then return end
-    local edit = result[1].edit
-    vim.lsp.util.apply_workspace_edit(edit)
+    for _, result in pairs(result) do
+        if not result.result then return end
+        result = result.result
+        local edit = result[1].edit
+        vim.lsp.util.apply_workspace_edit(edit)
+    end
 end
 
 vim.api.nvim_command([[
