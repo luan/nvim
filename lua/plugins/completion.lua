d80ad50c85b01d143762e1a1b22a01e6e515cf81
Luan Santos <luan@dropbox.com>
2021-10-16 10:41:21 -0700

    fix tab binding during completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -24,28 +24,27 @@ cmp.setup({
 	vim.fn.feedkeys(t('<Plug>CloserClose'), '')
       end
     end),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-	vim.fn.feedkeys(t('<C-n>'), 'n')
-      elseif check_back_space() then
-	vim.fn.feedkeys(t('<Tab>'), 'n')
-      elseif vim.fn['vsnip#available']() == 1 then
-	vim.fn.feedkeys(t('<Plug>(vsnip-expand-or-jump)'), '')
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"]() == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
       else
-	fallback()
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-	vim.fn.feedkeys(t('<C-p>'), 'n')
-      elseif check_back_space() then
-	vim.fn.feedkeys(t('<S-Tab>'), 'n')
-      elseif vim.fn['vsnip#available']() == 1 then
-	vim.fn.feedkeys(t('<Plug>(vsnip-prev)'), '')
-      else
-	fallback()
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
-    end, {'i', 's'}),
+    end, { "i", "s" }),
+
     ['<C-j>'] = cmp.mapping(function(fallback)
       if vim.fn['vsnip#available']() == 1 then
 	vim.fn.feedkeys(t('<Plug>(vsnip-expand-or-jump)'), '')
