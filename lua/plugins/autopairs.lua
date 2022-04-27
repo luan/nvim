6ab778fb70b6390682019618a590aad210f0da8a
Luan Santos <cfcluan@gmail.com>
2022-04-27 14:17:17 -0500

    ad missing autopairs cofig

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+require('nvim-autopairs').setup{}
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+-- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
+cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
