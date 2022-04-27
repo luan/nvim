3c044cb69e1d7e66321fbefd072e146f424e7c47
Luan Santos <cfcluan@gmail.com>
2022-04-27 14:32:26 -0500

    check for nvim 0.7.0

--- init.lua
+++ init.lua
@@ -6,12 +6,20 @@ vim.g.loaded_logipat          = 1
 vim.g.loaded_rrhelper         = 1
 vim.g.loaded_vimballPlugin    = 1
 
+if vim.version().major == 0 and vim.version().minor < 7 then
+    local fmt = string.format
+    local version = vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
+    vim.api.nvim_err_writeln(
+        fmt('luan/nvim requires at least nvim-0.7.0 (current %s)', version)
+    )
+    vim.api.nvim_err_writeln('Please update Neovim')
+    return
+end
+
 require('globals')
 require('packer-init')
 
 local has_module  = require('utils').has_module
-local file_exists = require('utils').file_exists
-local copy = require('utils').copy
 
 if has_module('user.before') then
     require('user.before')
