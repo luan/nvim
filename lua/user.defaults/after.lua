61fce4bd88a15bf33d60c99d605517518b3c32dd
Luan Santos <github@luan.sh>
2022-05-22 12:42:27 -0700

    Delete lua/user.defaults directory
    This just causes confusion and doesn't really seem to help self-documenting.
    
    Closes #89

--- lua/user.defaults/after.lua
+++
@@ -1,7 +0,0 @@
--- Called after everything just before setting a default colorscheme
--- Configure you own bindings or other preferences. e.g.:
---
--- vim.opt.number = false -- No line numbers
--- require('utils').map('n', 's', ':MultipleCursorsFind<cr>')
--- ...
---
