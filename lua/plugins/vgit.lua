ec07d96fd22119fef431e42623cae2b9114021e0
Luan Santos <cfcluan@gmail.com>
2022-06-09 22:39:45 -0700

    dont bind vgit

--- lua/plugins/vgit.lua
+++ lua/plugins/vgit.lua
@@ -1,4 +1,8 @@
 require('vgit').setup {
+  keymaps = {
+    ['n <C-k>'] = 'hunk_up',
+    ['n <C-j>'] = 'hunk_down',
+  },
   settings = {
     authorship_code_lens = { enabled = false },
     live_blame = { enabled = false },
