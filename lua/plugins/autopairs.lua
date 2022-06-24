3ee7a58746b6c742662bc55b068678e01da540e0
Luan Santos <git@luan.sh>
2022-06-24 09:52:47 -0700

    no more racket

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,8 +1,5 @@
-require('nvim-autopairs').setup{}
+require('nvim-autopairs').setup {}
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-
--- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
-cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
