c85c18782e785261ad24702f22ac3c2822f806f8
Luan Santos <github@luan.sh>
2022-08-04 09:49:05 -0700

    setup file browser in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,4 @@
 local actions = require('telescope.actions')
-local trouble = require("trouble.providers.telescope")
 
 require("telescope").setup {
     defaults = {
@@ -39,7 +38,19 @@ require("telescope").setup {
             case_mode = "smart_case" -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
         },
+        file_browser = {
+            theme = "ivy",
+            mappings = {
+                ["i"] = {
+                    -- your custom insert mode mappings
+                },
+                ["n"] = {
+                    -- your custom normal mode mappings
+                },
+            },
+        },
     }
 }
 
 require("telescope").load_extension("fzf")
+require("telescope").load_extension "file_browser"
