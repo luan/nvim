140228770874c332d83a5233cc0542c69006e95d
Luan Santos <github@luan.sh>
2022-08-04 10:04:11 -0700

    show hidden by default #90

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,5 +1,5 @@
 if vim.fn.executable('fd') == 1 then
-  vim.env.FZF_DEFAULT_COMMAND = 'fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f'
+  vim.env.FZF_DEFAULT_COMMAND = 'fd --hidden --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f'
 elseif vim.fn.executable('rg') == 1 then
   vim.env.FZF_DEFAULT_COMMAND = 'rg --files --hidden --follow --glob "!.git/*" --glob "!node_modules/*"'
 elseif vim.fn.executable('ag') == 1 then
@@ -9,19 +9,19 @@ end
 vim.g.fzf_layout = { window = { width = 0.9, height = 0.6 } }
 
 vim.g.fzf_colors = {
-    ['fg'] =       {'fg', 'Normal'},
-    ['bg'] =       {'bg', 'Normal'},
-    ['hl'] =       {'fg', 'Comment'},
-    ['fg+'] =      {'fg', 'CursorLine', 'CursorColumn', 'Normal'},
-    ['bg+'] =      {'bg', 'CursorLine', 'CursorColumn'},
-    ['hl+'] =      {'fg', 'Statement'},
-    ['info'] =     {'fg', 'PreProc'},
-    ['border'] =   {'fg', 'Ignore'},
-    ['prompt'] =   {'fg', 'Conditional'},
-    ['pointer'] =  {'fg', 'Exception'},
-    ['marker'] =   {'fg', 'Keyword'},
-    ['spinner'] =  {'fg', 'Label'},
-    ['header'] =   {'fg', 'Comment'},
+  ['fg'] = { 'fg', 'Normal' },
+  ['bg'] = { 'bg', 'Normal' },
+  ['hl'] = { 'fg', 'Comment' },
+  ['fg+'] = { 'fg', 'CursorLine', 'CursorColumn', 'Normal' },
+  ['bg+'] = { 'bg', 'CursorLine', 'CursorColumn' },
+  ['hl+'] = { 'fg', 'Statement' },
+  ['info'] = { 'fg', 'PreProc' },
+  ['border'] = { 'fg', 'Ignore' },
+  ['prompt'] = { 'fg', 'Conditional' },
+  ['pointer'] = { 'fg', 'Exception' },
+  ['marker'] = { 'fg', 'Keyword' },
+  ['spinner'] = { 'fg', 'Label' },
+  ['header'] = { 'fg', 'Comment' },
 }
 
 vim.g.fzf_history_dir = '~/.local/share/fzf-history'
