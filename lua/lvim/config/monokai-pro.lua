6fe65911a212839a7f14500ffb2763d22eb85e10
Luan Santos <github@luan.sh>
2023-01-27 14:36:38 -0800

    better tabs

--- lua/lvim/config/monokai-pro.lua
+++ lua/lvim/config/monokai-pro.lua
@@ -1,25 +1,38 @@
-local monokai = require("monokai-pro")
-monokai.setup({
-  transparent_background = true,
-  italic_comments = true,
-  filter = "octagon", -- classic | octagon | pro | machine | ristretto | spectrum
-  inc_search = "underline", -- underline | background
-  background_clear = {},
-  diagnostic = {
-    background = true,
-  },
-  plugins = {
-    bufferline = {
-      underline_selected = true,
-      underline_visible = false,
-    },
-    indent_blankline = {
-      context_highlight = "pro", -- default | pro
-    },
-    lualine = {
-      float = true,
-      colorful = true,
-    }
-  },
-})
+local monokai = require "monokai-pro"
+monokai.setup {
+   transparent_background = true,
+   italic_comments = true,
+   filter = "octagon", -- classic | octagon | pro | machine | ristretto | spectrum
+   inc_search = "underline", -- underline | background
+   background_clear = {},
+   diagnostic = {
+      background = true,
+   },
+   plugins = {
+      bufferline = {
+         underline_selected = false,
+         underline_visible = false,
+      },
+      indent_blankline = {
+         context_highlight = "pro", -- default | pro
+      },
+      lualine = {
+         float = true,
+         colorful = true,
+      },
+   },
+}
+
+local highlight = function(group, value)
+   local link = value.link
+   if link ~= nil then
+      local cmd = "hi! link " .. group .. " " .. link
+      vim.api.nvim_command(cmd)
+      return
+   end
+   local generatedHlValue = genHlValue(value)
+   vim.api.nvim_set_hl(0, group, generatedHlValue)
+end
+
 monokai.load()
+vim.api.nvim_command "hi IndentBlanklineContextStart gui=underdotted"
