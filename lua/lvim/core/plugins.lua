36d0c43a46c82f45af1e9777746c5562128ab6e0
Luan Santos <github@luan.sh>
2023-02-03 17:18:03 -0800

    lazy load snippets

--- lua/lvim/core/plugins.lua
+++ lua/lvim/core/plugins.lua
@@ -122,9 +122,13 @@ local groups = {
   },
 
   code = {
-    "L3MON4D3/LuaSnip",
+    {
+      "L3MON4D3/LuaSnip",
+      event = "InsertEnter",
+    },
 
     {
+      event = "InsertEnter",
       "rafamadriz/friendly-snippets",
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
