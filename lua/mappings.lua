228af642d7be2e61b01110b6344dccda1610b09c
Luan Santos <git@luan.sh>
2024-11-12 07:40:22 -0800

    feat: avante

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,11 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", "<c-p>", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+map("n", "x", "V")
+map("v", "x", "j")
+map("n", "<cr>", "<cmd> w <cr>")
+map("n", "<C-c>", "gcc", { desc = "toggle comment", remap = true })
+map("x", "<C-c>", "gc", { desc = "toggle comment", remap = true })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
