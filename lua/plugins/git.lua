f157ca2e513d164861780a96fe8a48d9a644ed23
Luan Santos <luan@thebrowser.company>
2025-07-19 20:20:41 -0700

    feat: enhance config with new plugins and improved keymaps
    - Add blink.lua, multicursor.nvim, and snacks-picker plugins
    - Switch to local fugit2 development version with diffview integration
    - Update Claude Code keymaps to use Cmd key for better macOS experience
    - Add multicursor support and improve buffer navigation
    - Configure socket management for better project isolation

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,16 +2,31 @@ return {
   { "NeogitOrg/neogit" },
   { "tpope/vim-fugitive" },
   { "tpope/vim-rhubarb" },
+  { "nvim-lua/plenary.nvim" },
   {
-    "SuperBo/fugit2.nvim",
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- lazy, only load diffview by these commands
+    cmd = {
+      "DiffviewFileHistory",
+      "DiffviewOpen",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewRefresh",
+    },
+  },
+  {
+    dir = "~/src/fugit2.nvim",
+    name = "fugit2.nvim",
+    dev = true,
     build = false,
     opts = {
       width = 100,
+      external_diffview = true,
     },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons",
-      "nvim-lua/plenary.nvim",
       {
         "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
         dependencies = { "stevearc/dressing.nvim" },
@@ -21,6 +36,9 @@ return {
     keys = {
       { "<leader>gg", mode = "n", "<cmd>Fugit2<cr>" },
       { "<leader>gb", mode = "n", "<cmd>Fugit2Blame<cr>" },
+      { "<leader>gd", mode = "n", "<cmd>DiffviewOpen<cr>" },
+      { "<leader>gc", mode = "n", "<cmd>DiffviewClose<cr>" },
+      { "<leader>gh", mode = "n", "<cmd>DiffviewFileHistory %<cr>" },
     },
   },
 }
---
+++ lua/plugins/multicursor.lua
@@ -0,0 +1,76 @@
+return {
+  "jake-stewart/multicursor.nvim",
+  branch = "1.0",
+  config = function()
+    local mc = require("multicursor-nvim")
+    mc.setup()
+
+    local set = vim.keymap.set
+
+    -- Add or skip cursor above/below the main cursor.
+    set({ "n", "x" }, "<D-M-k>", function()
+      mc.lineAddCursor(-1)
+    end)
+    set({ "n", "x" }, "<D-M-j>", function()
+      mc.lineAddCursor(1)
+    end)
+    set({ "n", "x" }, "<D-M-S-k>", function()
+      mc.lineSkipCursor(-1)
+    end)
+    set({ "n", "x" }, "<D-M-S-j>", function()
+      mc.lineSkipCursor(1)
+    end)
+
+    -- Add or skip adding a new cursor by matching word/selection
+    set({ "n", "x" }, "<D-d>", function()
+      mc.matchAddCursor(1)
+    end)
+    set({ "n", "x" }, "<D-S-d>", function()
+      mc.matchSkipCursor(1)
+    end)
+    set({ "n", "x" }, "<D-u>", function()
+      mc.matchAddCursor(-1)
+    end)
+    set({ "n", "x" }, "<D-S-u>", function()
+      mc.matchSkipCursor(-1)
+    end)
+
+    -- Add and remove cursors with control + left click.
+    set("n", "<c-leftmouse>", mc.handleMouse)
+    set("n", "<c-leftdrag>", mc.handleMouseDrag)
+    set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+    -- Disable and enable cursors.
+    set({ "n", "x" }, "<c-q>", mc.toggleCursor)
+
+    -- Mappings defined in a keymap layer only apply when there are
+    -- multiple cursors. This lets you have overlapping mappings.
+    mc.addKeymapLayer(function(layerSet)
+      -- Select a different cursor as the main one.
+      layerSet({ "n", "x" }, "<D-k>", mc.prevCursor)
+      layerSet({ "n", "x" }, "<D-j>", mc.nextCursor)
+
+      -- Delete the main cursor.
+      layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+      -- Enable and clear cursors using escape.
+      layerSet("n", "<esc>", function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        else
+          mc.clearCursors()
+        end
+      end)
+    end)
+
+    -- Customize how cursors look.
+    local hl = vim.api.nvim_set_hl
+    hl(0, "MultiCursorCursor", { reverse = true })
+    hl(0, "MultiCursorVisual", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn" })
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { reverse = true })
+    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+  end,
+}
