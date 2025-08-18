c8b8a7f08e05a231f89bba116e7cd1d2ce3404d6
Luan Santos <github@luan.sh>
2025-08-17 17:40:58 -0700

    feat: dropbar

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,31 @@
+return {
+  "Bekaboo/dropbar.nvim",
+  -- optional, but required for fuzzy finder support
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+  opts = {},
+  config = function()
+    local dropbar_api = require("dropbar.api")
+    vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+    vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+    vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+
+    local dropbar = require("dropbar")
+    dropbar.setup({
+      icons = {
+        ui = {
+          bar = {
+            separator = "   ",
+          },
+        },
+      },
+      menu = {
+        scrollbar = {
+          background = false,
+        },
+      },
+    })
+  end,
+}
