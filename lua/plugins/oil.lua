3ebe6daf9d3d452b487297617f1bdb7aede43851
Luan Santos <github@luan.sh>
2025-08-17 17:40:48 -0700

    clean: split up some basic functins

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      watch_for_changes = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = false,
+      default_file_explorer = true,
+      keymaps = {
+        ["gd"] = function()
+          require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+        end,
+        ["gD"] = function()
+          require("oil").set_columns({ "icon" })
+        end,
+      },
+    },
+    -- Optional dependencies
+    dependencies = {
+      { "echasnovski/mini.icons", opts = {} },
+      { "benomahony/oil-git.nvim" },
+    },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      {
+        "-",
+        function()
+          require("oil").open()
+        end,
+        desc = "Open parent directory in Oil",
+      },
+    },
+  },
+}
