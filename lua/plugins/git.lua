1beaec9d9662ed88e5378f13e53cbfe49fdfa4c1
Luan Santos <git@luan.sh>
2026-02-04 10:27:22 -0800

    feat(review): integrate with lazygit via alt+r
    Use luan/review.nvim fork with rev args support. When alt+r pressed
    in lazygit commits view, writes commit to temp file, quits lazygit,
    and nvim TermClose autocmd opens review.nvim with that commit.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,34 @@
+local review_request_file = "/tmp/nvim-review-request"
+
+local function setup_lazygit_review_hook()
+  vim.api.nvim_create_autocmd("TermClose", {
+    pattern = "*lazygit*",
+    callback = function()
+      vim.defer_fn(function()
+        local f = io.open(review_request_file, "r")
+        if not f then
+          return
+        end
+        local content = f:read("*a")
+        f:close()
+        os.remove(review_request_file)
+
+        local lines = vim.split(content, "\n", { trimempty = true })
+        if #lines >= 2 then
+          require("review").open_commits(lines[1], lines[2])
+        elseif #lines == 1 then
+          require("review").open_commits(lines[1] .. "^", lines[1])
+        end
+      end, 100)
+    end,
+  })
+end
+
 return {
   { "tpope/vim-fugitive" },
   { "tpope/vim-rhubarb" },
   {
-    "georgeguimaraes/review.nvim",
+    "luan/review.nvim",
     dependencies = {
       "esmuellert/codediff.nvim",
       "MunifTanjim/nui.nvim",
@@ -15,5 +41,9 @@ return {
       { "<leader>gS", "<cmd>Review sidekick<cr>", desc = "Send review to sidekick" },
     },
     opts = {},
+    config = function(_, opts)
+      require("review").setup(opts)
+      setup_lazygit_review_hook()
+    end,
   },
 }
