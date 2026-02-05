942a2b9d8c7d3b13cd7e276dbb913bd0825947d8
Luan Santos <git@luan.sh>
2026-02-05 10:17:59 -0800

    feat(review): use fork with lazygit integration and popup fixes
    - Point to luan/review.nvim fork with open_commits rev args support
    - Add custom popup_submit keymap (<C-j>)
    - Remove unused TermClose hook (now using nvim --remote-send)

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,29 +1,3 @@
-local review_request_file = "/tmp/nvim-review-request"
-
-local function setup_lazygit_review_hook()
-  vim.api.nvim_create_autocmd("TermClose", {
-    pattern = "*lazygit*",
-    callback = function()
-      vim.defer_fn(function()
-        local f = io.open(review_request_file, "r")
-        if not f then
-          return
-        end
-        local content = f:read("*a")
-        f:close()
-        os.remove(review_request_file)
-
-        local lines = vim.split(content, "\n", { trimempty = true })
-        if #lines >= 2 then
-          require("review").open_commits(lines[1], lines[2])
-        elseif #lines == 1 then
-          require("review").open_commits(lines[1] .. "^", lines[1])
-        end
-      end, 100)
-    end,
-  })
-end
-
 return {
   { "tpope/vim-fugitive" },
   { "tpope/vim-rhubarb" },
@@ -40,10 +14,10 @@ return {
       { "<leader>gP", "<cmd>Review preview<cr>", desc = "Preview review" },
       { "<leader>gS", "<cmd>Review sidekick<cr>", desc = "Send review to sidekick" },
     },
-    opts = {},
-    config = function(_, opts)
-      require("review").setup(opts)
-      setup_lazygit_review_hook()
-    end,
+    opts = {
+      keymaps = {
+        popup_submit = "<C-j>",
+      },
+    },
   },
 }
